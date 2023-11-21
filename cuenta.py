fc251b15ea2470a4ce022ffc0e64bb84489a85ae
AlbaGome <albaliriagomezmontano@gmail.com>
2023-11-21 16:41:07 -0400

    2023-11_21_01

--- cuenta.py
+++ cuenta.py
@@ -1,21 +1,19 @@
 class Cuenta:
     def __init__(self,numero, titular,saldo,limite):
         print("Construyendo el objeto...{}".format(self))
-        self.numero =numero
-        self.titular =titular
-        self.saldo=saldo
-        self.limite=limite 
+        self._numero =numero
+        self._titular =titular
+        self._saldo=saldo
+        self._limite=limite 
         
     def extracto(self):
-        print('saldo {} del titular {}'.format (self.saldo,self.titular))
+        print('saldo {} del titular {}'.format (self._saldo,self._titular))
         
         
     def depositar (self,valor):
-        self.saldo += valor
+        self._saldo += valor
         
          
     def retirar(self, valor):
-        if(valor<= self.saldo):
-            self.saldo -= valor
-        else:
-            print(" Usted no tiene suficiente saldo")
+        self._saldo -= valor
+       
